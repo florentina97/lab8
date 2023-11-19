161351def89930b77c4d57cccc2c308170e534fe
Mihaela <florentinamihaelaj@icloud.com>
2023-11-19 11:38:04 +0000

    first commit

---
+++ src/lib/api/categories/+server.js
@@ -0,0 +1,40 @@
+import { json } from '@sveltejs/kit';
+
+// import the client instance (created earlier)
+import {supabase} from "$lib/supabase.js";
+
+export async function GET({ request, cookies }) {
+
+    const categories = await supabase
+    .from('category')
+    .select('*')
+    .order('name', {ascending: true});
+
+    if (categories.error) {
+        return json({
+            status: categories.status,
+            error: categories.error
+        });
+    }
+
+    return json({
+        data: categories.data,
+        status: categories.status
+    });
+}
+
+export async function POST({ request, cookies }) {
+    // Get data from request body
+    let data = await request.json();
+
+    // Insert data
+    const cat = await supabase
+    .from('category')
+    .insert([data])
+    .select()
+
+
+    return json({
+        data: cat.data
+    });
+}
